8dfc78540fced8892fd5ceeae71e2357f9d46ee2
Capputino <pereirabragaleonardo8@gmail.com>
2025-05-19 22:11:21 -0300

    atualização do migrate_cadastro_produtos

--- database/migrations/2025_04_17_171151_create_produtos_table.php
+++ database/migrations/2025_04_17_171151_create_produtos_table.php
@@ -9,23 +9,28 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        // Tabela de Produtos
-        Schema::create('produtos', function (Blueprint $table) {
-            $table->id('id_produto');
-            $table->string('nome_produto');
-            $table->string('slug')->unique();
-            $table->text('descricao')->nullable();
-            $table->string('genero');
-            $table->decimal('preco', 10, 2);
-            // $table->unsignedBigInteger('id_categoria');
-            $table->string('img');
-            $table->timestamps();
+    public function up()
+{
+    
+    Schema::create('produtos', function (Blueprint $table) {
+        $table->id();
+        $table->string('nome');
+        $table->string('tipo');
+        $table->text('descricao');
+        $table->string('cor');
+        $table->string('estacao');
+        $table->string('marca');
+        $table->decimal('valor', 8, 2);
+        $table->string('tamanho');
+        $table->integer('estoque');
+        $table->string('tecido');
+        $table->string('modelo');
+        $table->string('imagem'); // Caminho da imagem
+        $table->timestamps();
+    });
+}
 
-            // $table->foreign('id_categoria')->references('id_categoria')->on('categorias')->onDelete('cascade');
-        });
-    }
+    
 
     /**
      * Reverse the migrations.
